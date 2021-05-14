29562266116c3ef823b903ddb0cba61684ad2dd5
JJLindsey <jlindsey010@gmail.com>
2021-05-14 12:18:24 -0500

    add conditions for mutliple choice

--- js/script.js
+++ js/script.js
@@ -11,24 +11,27 @@ var quizContainerEl = $('.quiz-container');
 //array for quiz questions
 var questions = [
     {
-    q: "What does JavaScript use to store data?", 
-    a: ["Strings", "Variables", "Booleans", "Barrels"],
-    correctAns: 1
-},
-{   q: "Functions take ___________________ to define or perform its task.", 
-    a: ["parameters", "obstactles", "assets", "values"],
-    correctAns: 0
-},
-    
-{   q: "True or False in JavaScript is called _______________.",
-    a: ["true", "condition", "not valid", "boolean"],
-    correctAns: 3
-},
-    
-{   q: "Items in an Array are stored in a numbered list called a(n) ______________.",
-    a: ["string", "drawer", "index", "variable"],
-    correctAns: 2
-}];
+        q: "What does JavaScript use to store data?",
+        a: ["Strings", "Variables", "Booleans", "Barrels"],
+        correctAns: 1
+    },
+    {
+        q: "Functions take ___________________ to define or perform its task.",
+        a: ["parameters", "obstactles", "assets", "values"],
+        correctAns: 0
+    },
+
+    {
+        q: "True or False in JavaScript is called _______________.",
+        a: ["true", "condition", "not valid", "boolean"],
+        correctAns: 3
+    },
+
+    {
+        q: "Items in an Array are stored in a numbered list called a(n) ______________.",
+        a: ["string", "drawer", "index", "variable"],
+        correctAns: 2
+    }];
 
 
 var score = 0;
@@ -64,7 +67,7 @@ function displayQuestion(questionObject) {
     var optionsLi4 = $('<li>');
     optionsLi4.addClass('list-group-item');
     optionsLi4.text(questionObject.a[3]);
-    
+
     optionsUl.append(optionsLi)
     optionsUl.append(optionsLi2)
     optionsUl.append(optionsLi3)
@@ -77,8 +80,8 @@ function displayQuestion(questionObject) {
 
 displayQuestion(questions[0])
 
-    
-    //<h3 id="question-text">Sample question</h3>
+
+//<h3 id="question-text">Sample question</h3>
 // <div class="card" style="width: 18rem;">
 //     <ul class="list-group list-group-flush">
 //       <li class="list-group-item">Cras justo odio</li>
@@ -94,26 +97,40 @@ for (var i = 0; i < questions.length; i++) {
     // Display current question to user as user to choose correct answer
     var answer = (questions[i].q);
 
+    // Compare answers
+    if(answer === questions[i].correctAns){
+        // add to the number of correct answers
+        numCorrect++;
+        
+        // color the answers green
+        optionsUl[i].style.color = 'lightgreen';
+    }
+    // if answer is wrong or blank
+    else{
+        // color the answers red
+        optionsUl[i].style.color = 'red';
+    }
 }
+
 
 // Timer that counts down from 60
 function countdown() {
     var timeLeft = 60;
 
-  // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
     var timeInterval = setInterval(function () {
-    if(timeLeft >= 1) {
+        if (timeLeft >= 1) {
 
-        //timerEl.textContent = timeLeft + ' seconds remaining';
-        timerEl.text(timeLeft + ' seconds remaining');
-        timeLeft--;
-    } else {
-        timerEl.text('');
-        //timerEl.textContent = '';
-        clearInterval(timeInterval);
-        displayMessage();
-    }
-   //every second run above code until timeLeft = 0
+            //timerEl.textContent = timeLeft + ' seconds remaining';
+            timerEl.text(timeLeft + ' seconds remaining');
+            timeLeft--;
+        } else {
+            timerEl.text('');
+            //timerEl.textContent = '';
+            clearInterval(timeInterval);
+            displayMessage();
+        }
+        //every second run above code until timeLeft = 0
     }, 1000);
 }
 
